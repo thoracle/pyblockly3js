4342b6c9ca9be431985f00cc6aacebfef9375b47
thoracle <thoralexander310@gmail.com>
2025-05-05 20:24:11 -0700

    color ground plane working

--- static/js/blockly.js
+++ static/js/blockly.js
@@ -3,95 +3,51 @@ console.log('Loading Blockly block definitions...');
 
 // Define custom block types
 const customBlocks = {
-    // Color picker block
+    // Scene control blocks
     scene_color_picker: {
         init: function() {
             this.appendDummyInput()
-                .appendField("Set scene color")
+                .appendField("Set scene background color")
                 .appendField(new Blockly.FieldColour("#ffffff"), "COLOR");
             this.setPreviousStatement(true, null);
             this.setNextStatement(true, null);
-            this.setColour(230);
+            this.setColour(160);
             this.setTooltip("Set the scene background color");
         }
     },
-    // Movement blocks
-    move_forward: {
+    set_ground_color: {
         init: function() {
             this.appendDummyInput()
-                .appendField("move forward");
+                .appendField("Set ground color")
+                .appendField(new Blockly.FieldColour("#808080"), "COLOR");
             this.setPreviousStatement(true, null);
             this.setNextStatement(true, null);
-            this.setColour(230);
-            this.setTooltip("Move the object forward");
+            this.setColour(160);
+            this.setTooltip("Set the color of the ground plane");
         }
     },
-    move_backward: {
+    set_ground_size: {
         init: function() {
-            this.appendDummyInput()
-                .appendField("move backward");
-            this.setPreviousStatement(true, null);
-            this.setNextStatement(true, null);
-            this.setColour(230);
-            this.setTooltip("Move the object backward");
-        }
-    },
-    turn_left: {
-        init: function() {
-            this.appendDummyInput()
-                .appendField("turn left");
-            this.setPreviousStatement(true, null);
-            this.setNextStatement(true, null);
-            this.setColour(230);
-            this.setTooltip("Turn the object left");
-        }
-    },
-    turn_right: {
-        init: function() {
-            this.appendDummyInput()
-                .appendField("turn right");
-            this.setPreviousStatement(true, null);
-            this.setNextStatement(true, null);
-            this.setColour(230);
-            this.setTooltip("Turn the object right");
-        }
-    },
-    move_distance: {
-        init: function() {
-            this.appendValueInput("DISTANCE")
+            this.appendValueInput("SIZE")
                 .setCheck("Number")
-                .appendField("move")
-                .appendField("units");
+                .appendField("Set ground size to");
             this.setPreviousStatement(true, null);
             this.setNextStatement(true, null);
-            this.setColour(230);
-            this.setTooltip("Move the object a specific distance");
+            this.setColour(160);
+            this.setTooltip("Set the size of the ground plane");
         }
     },
-    turn_degrees: {
-        init: function() {
-            this.appendValueInput("DEGREES")
-                .setCheck("Number")
-                .appendField("turn")
-                .appendField("degrees");
-            this.setPreviousStatement(true, null);
-            this.setNextStatement(true, null);
-            this.setColour(230);
-            this.setTooltip("Turn the object a specific number of degrees");
-        }
-    },
-    // Scene control blocks
     set_camera_position: {
         init: function() {
             this.appendValueInput("X")
                 .setCheck("Number")
-                .appendField("set camera position x");
+                .appendField("Set camera position X");
             this.appendValueInput("Y")
                 .setCheck("Number")
-                .appendField("y");
+                .appendField("Y");
             this.appendValueInput("Z")
                 .setCheck("Number")
-                .appendField("z");
+                .appendField("Z");
             this.setPreviousStatement(true, null);
             this.setNextStatement(true, null);
             this.setColour(160);
@@ -102,13 +58,13 @@ const customBlocks = {
         init: function() {
             this.appendValueInput("X")
                 .setCheck("Number")
-                .appendField("set camera to look at x");
+                .appendField("Set camera to look at X");
             this.appendValueInput("Y")
                 .setCheck("Number")
-                .appendField("y");
+                .appendField("Y");
             this.appendValueInput("Z")
                 .setCheck("Number")
-                .appendField("z");
+                .appendField("Z");
             this.setPreviousStatement(true, null);
             this.setNextStatement(true, null);
             this.setColour(160);
@@ -117,9 +73,9 @@ const customBlocks = {
     },
     set_light_color: {
         init: function() {
-            this.appendValueInput("COLOR")
-                .setCheck("Colour")
-                .appendField("set light color to");
+            this.appendDummyInput()
+                .appendField("Set directional light color")
+                .appendField(new Blockly.FieldColour("#ffffff"), "COLOR");
             this.setPreviousStatement(true, null);
             this.setNextStatement(true, null);
             this.setColour(160);
@@ -130,39 +86,60 @@ const customBlocks = {
         init: function() {
             this.appendValueInput("INTENSITY")
                 .setCheck("Number")
-                .appendField("set light intensity to");
+                .appendField("Set directional light intensity to");
             this.setPreviousStatement(true, null);
             this.setNextStatement(true, null);
             this.setColour(160);
             this.setTooltip("Set the intensity of the directional light (0-1)");
         }
     },
-    set_ground_color: {
+    set_light_position: {
         init: function() {
-            this.appendValueInput("COLOR")
-                .setCheck("Colour")
-                .appendField("set ground color to");
+            this.appendValueInput("X")
+                .setCheck("Number")
+                .appendField("Set light position X");
+            this.appendValueInput("Y")
+                .setCheck("Number")
+                .appendField("Y");
+            this.appendValueInput("Z")
+                .setCheck("Number")
+                .appendField("Z");
             this.setPreviousStatement(true, null);
             this.setNextStatement(true, null);
             this.setColour(160);
-            this.setTooltip("Set the color of the ground plane");
-            this.setInputsInline(true);
-            
-            // Add a default color picker block
-            const colorBlock = workspace.newBlock('scene_color_picker');
-            colorBlock.initSvg();
-            this.getInput('COLOR').connection.connect(colorBlock.outputConnection);
+            this.setTooltip("Set the position of the directional light");
         }
     },
-    set_ground_size: {
+    set_ambient_light_color: {
         init: function() {
-            this.appendValueInput("SIZE")
-                .setCheck("Number")
-                .appendField("set ground size to");
+            this.appendDummyInput()
+                .appendField("Set ambient light color")
+                .appendField(new Blockly.FieldColour("#ffffff"), "COLOR");
             this.setPreviousStatement(true, null);
             this.setNextStatement(true, null);
             this.setColour(160);
-            this.setTooltip("Set the size of the ground plane");
+            this.setTooltip("Set the color of the ambient light");
+        }
+    },
+    set_ambient_light_intensity: {
+        init: function() {
+            this.appendValueInput("INTENSITY")
+                .setCheck("Number")
+                .appendField("Set ambient light intensity to");
+            this.setPreviousStatement(true, null);
+            this.setNextStatement(true, null);
+            this.setColour(160);
+            this.setTooltip("Set the intensity of the ambient light (0-1)");
+        }
+    },
+    reset_scene: {
+        init: function() {
+            this.appendDummyInput()
+                .appendField("Reset scene to default");
+            this.setPreviousStatement(true, null);
+            this.setNextStatement(true, null);
+            this.setColour(160);
+            this.setTooltip("Reset the scene to its default state");
         }
     }
 };
@@ -173,86 +150,50 @@ const generators = {
         const color = block.getFieldValue('COLOR');
         return `scene.background = new THREE.Color('${color}');\n`;
     },
-    move_forward: function(block) {
-        return 'moveForward();\n';
+    set_ground_color: function(block) {
+        const color = block.getFieldValue('COLOR');
+        return `threeApp.setGroundColor('${color}');\n`;
     },
-    move_backward: function(block) {
-        return 'moveBackward();\n';
+    set_ground_size: function(block) {
+        const size = Blockly.JavaScript.valueToCode(block, 'SIZE', Blockly.JavaScript.ORDER_ATOMIC);
+        return `threeApp.setGroundSize(${size});\n`;
     },
-    turn_left: function(block) {
-        return 'turnLeft();\n';
-    },
-    turn_right: function(block) {
-        return 'turnRight();\n';
-    },
-    move_distance: function(block) {
-        const distance = Blockly.JavaScript.valueToCode(block, 'DISTANCE', Blockly.JavaScript.ORDER_ATOMIC);
-        if (!distance) {
-            console.error('Missing distance value in move_distance block');
-            return 'console.error("Missing distance value");\n';
-        }
-        return `moveDistance(${distance});\n`;
-    },
-    turn_degrees: function(block) {
-        const degrees = Blockly.JavaScript.valueToCode(block, 'DEGREES', Blockly.JavaScript.ORDER_ATOMIC);
-        if (!degrees) {
-            console.error('Missing degrees value in turn_degrees block');
-            return 'console.error("Missing degrees value");\n';
-        }
-        return `turnDegrees(${degrees});\n`;
-    },
-    // Scene control generators
     set_camera_position: function(block) {
         const x = Blockly.JavaScript.valueToCode(block, 'X', Blockly.JavaScript.ORDER_ATOMIC);
         const y = Blockly.JavaScript.valueToCode(block, 'Y', Blockly.JavaScript.ORDER_ATOMIC);
         const z = Blockly.JavaScript.valueToCode(block, 'Z', Blockly.JavaScript.ORDER_ATOMIC);
-        if (!x || !y || !z) {
-            console.error('Missing position values in set_camera_position block');
-            return 'console.error("Missing position values");\n';
-        }
-        return `setCameraPosition(${x}, ${y}, ${z});\n`;
+        return `threeApp.setCameraPosition(${x}, ${y}, ${z});\n`;
     },
     set_camera_look_at: function(block) {
         const x = Blockly.JavaScript.valueToCode(block, 'X', Blockly.JavaScript.ORDER_ATOMIC);
         const y = Blockly.JavaScript.valueToCode(block, 'Y', Blockly.JavaScript.ORDER_ATOMIC);
         const z = Blockly.JavaScript.valueToCode(block, 'Z', Blockly.JavaScript.ORDER_ATOMIC);
-        if (!x || !y || !z) {
-            console.error('Missing look at values in set_camera_look_at block');
-            return 'console.error("Missing look at values");\n';
-        }
-        return `setCameraLookAt(${x}, ${y}, ${z});\n`;
+        return `threeApp.setCameraLookAt(${x}, ${y}, ${z});\n`;
     },
     set_light_color: function(block) {
-        const color = Blockly.JavaScript.valueToCode(block, 'COLOR', Blockly.JavaScript.ORDER_ATOMIC);
-        if (!color) {
-            console.error('Missing color value in set_light_color block');
-            return 'console.error("Missing color value");\n';
-        }
-        return `setLightColor(${color});\n`;
+        const color = block.getFieldValue('COLOR');
+        return `threeApp.setDirectionalLightColor('${color}');\n`;
     },
     set_light_intensity: function(block) {
         const intensity = Blockly.JavaScript.valueToCode(block, 'INTENSITY', Blockly.JavaScript.ORDER_ATOMIC);
-        if (!intensity) {
-            console.error('Missing intensity value in set_light_intensity block');
-            return 'console.error("Missing intensity value");\n';
-        }
-        return `setLightIntensity(${intensity});\n`;
+        return `threeApp.setDirectionalLightIntensity(${intensity});\n`;
     },
-    set_ground_color: function(block) {
-        const color = Blockly.JavaScript.valueToCode(block, 'COLOR', Blockly.JavaScript.ORDER_ATOMIC);
-        if (!color) {
-            console.error('Missing color value in set_ground_color block');
-            return 'console.error("Missing color value");\n';
-        }
-        return `setGroundColor(${color});\n`;
+    set_light_position: function(block) {
+        const x = Blockly.JavaScript.valueToCode(block, 'X', Blockly.JavaScript.ORDER_ATOMIC);
+        const y = Blockly.JavaScript.valueToCode(block, 'Y', Blockly.JavaScript.ORDER_ATOMIC);
+        const z = Blockly.JavaScript.valueToCode(block, 'Z', Blockly.JavaScript.ORDER_ATOMIC);
+        return `threeApp.setDirectionalLightPosition(${x}, ${y}, ${z});\n`;
     },
-    set_ground_size: function(block) {
-        const size = Blockly.JavaScript.valueToCode(block, 'SIZE', Blockly.JavaScript.ORDER_ATOMIC);
-        if (!size) {
-            console.error('Missing size value in set_ground_size block');
-            return 'console.error("Missing size value");\n';
-        }
-        return `setGroundSize(${size});\n`;
+    set_ambient_light_color: function(block) {
+        const color = block.getFieldValue('COLOR');
+        return `threeApp.setAmbientLightColor('${color}');\n`;
+    },
+    set_ambient_light_intensity: function(block) {
+        const intensity = Blockly.JavaScript.valueToCode(block, 'INTENSITY', Blockly.JavaScript.ORDER_ATOMIC);
+        return `threeApp.setAmbientLightIntensity(${intensity});\n`;
+    },
+    reset_scene: function(block) {
+        return `threeApp.resetScene();\n`;
     }
 };
 
@@ -275,8 +216,8 @@ function initializeBlockly() {
     });
 
     // Verify block registration
-    console.log('Registered custom blocks:', Object.keys(Blockly.Blocks).filter(key => key.startsWith('scene_') || key.startsWith('move_') || key.startsWith('turn_') || key.startsWith('set_')));
-    console.log('Registered generators:', Object.keys(Blockly.JavaScript).filter(key => key.startsWith('scene_') || key.startsWith('move_') || key.startsWith('turn_') || key.startsWith('set_')));
+    console.log('Registered custom blocks:', Object.keys(Blockly.Blocks).filter(key => key.startsWith('scene_') || key.startsWith('set_')));
+    console.log('Registered generators:', Object.keys(Blockly.JavaScript).filter(key => key.startsWith('scene_') || key.startsWith('set_')));
 }
 
 // Wait for Blockly to be fully loaded
--- static/js/three.js
+++ static/js/three.js
@@ -1,6 +1,13 @@
 // Three.js application class
+let instance = null;
+
 class ThreeJSApp {
     constructor() {
+        if (instance) {
+            console.warn('ThreeJSApp instance already exists, returning existing instance');
+            return instance;
+        }
+        
         console.log('=== ThreeJSApp Constructor Start ===');
         this.scene = null;
         this.camera = null;
@@ -11,10 +18,16 @@ class ThreeJSApp {
         this.container = null;
         this.frameCount = 0;
         this.cube = null;
+        this.ground = null;
         this.directionalLight = null;
+        this.ambientLight = null;
         this.debugDisplay = null;
         this.showDebug = false;
+        this.materialNeedsUpdate = false;
         this.handleKeyDown = this.handleKeyDown.bind(this);
+        
+        // Store instance
+        instance = this;
         console.log('ThreeJSApp constructor completed');
         console.log('=== ThreeJSApp Constructor End ===');
     }
@@ -120,8 +133,8 @@ class ThreeJSApp {
         
         try {
             // Add ambient light
-            const ambientLight = new THREE.AmbientLight(0xffffff, 0.7);
-            this.scene.add(ambientLight);
+            this.ambientLight = new THREE.AmbientLight(0xffffff, 0.7);
+            this.scene.add(this.ambientLight);
             console.log('Ambient light added');
 
             // Add directional light
@@ -134,16 +147,16 @@ class ThreeJSApp {
             // Add ground plane
             const groundGeometry = new THREE.PlaneGeometry(20, 20);
             const groundMaterial = new THREE.MeshStandardMaterial({ 
-                color: 0x808080,
+                color: '#ffff00',  // Using hex string instead of number
                 side: THREE.DoubleSide,
                 roughness: 0.8,
                 metalness: 0.2
             });
-            const ground = new THREE.Mesh(groundGeometry, groundMaterial);
-            ground.rotation.x = -Math.PI / 2;
-            ground.receiveShadow = true;
-            this.scene.add(ground);
-            console.log('Ground plane added');
+            this.ground = new THREE.Mesh(groundGeometry, groundMaterial);
+            this.ground.rotation.x = -Math.PI / 2;
+            this.ground.receiveShadow = true;
+            this.scene.add(this.ground);
+            console.log('Ground plane added with yellow color:', this.ground.material.color);
 
             // Add a cube
             const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
@@ -308,54 +321,192 @@ class ThreeJSApp {
         }
 
         if (this.renderer && this.scene && this.camera) {
+            // Ensure material is properly initialized
+            if (this.ground && this.ground.material && this.materialNeedsUpdate) {
+                this.ground.material.needsUpdate = true;
+                this.materialNeedsUpdate = false;
+            }
+            
             this.renderer.render(this.scene, this.camera);
             this.frameCount++;
             
             // Update debug display
             this.updateDebugDisplay();
-            
-            // Log every 60 frames
-            if (this.frameCount % 60 === 0) {
-                console.log('Frame rendered:', {
-                    frameCount: this.frameCount,
-                    cameraPosition: this.camera.position,
-                    sceneChildren: this.scene.children.length,
-                    debugDisplayVisible: this.showDebug,
-                    debugDisplayExists: !!this.debugDisplay
-                });
-            }
         }
     }
 
     resetScene() {
-        console.log('Resetting scene...');
+        console.log('Resetting scene to default state');
         
-        // Reset cube position and rotation
+        // Reset cube
         if (this.cube) {
             this.cube.position.set(0, 0.5, 0);
             this.cube.rotation.set(0, 0, 0);
+            this.cube.material.color.setHex(0x00ff00);
         }
-
-        // Reset directional light
-        if (this.directionalLight) {
-            this.directionalLight.position.set(5, 15, 5);
-            this.directionalLight.intensity = 0.8;
-            this.directionalLight.color.set(0xffffff);
-        }
-
-        // Reset camera position and target
-        if (this.camera) {
-            this.camera.position.set(-30, 15, 15);
-            this.camera.lookAt(-3, -3, -15);
-        }
-
-        // Reset controls
+        
+        // Reset camera and controls
+        this.camera.position.set(-30, 15, 15);
+        this.camera.lookAt(-3, -3, -15);
         if (this.controls) {
             this.controls.target.set(-3, -3, -15);
             this.controls.update();
         }
+        
+        // Reset directional light
+        if (this.directionalLight) {
+            this.directionalLight.position.set(5, 15, 5);
+            this.directionalLight.intensity = 0.8;
+            this.directionalLight.color.setHex(0xffffff);
+        }
+        
+        // Reset ambient light
+        if (this.ambientLight) {
+            this.ambientLight.intensity = 0.7;
+            this.ambientLight.color.setHex(0xffffff);
+        }
+        
+        // Reset scene background
+        this.scene.background = new THREE.Color(0xf0f0f0);
+        
+        // Reset ground
+        if (this.ground) {
+            this.ground.material.color.setHex(0xffff00);
+        }
+    }
 
-        console.log('Scene reset complete');
+    // Scene operation functions
+    setGroundColor(colorValue) {
+        console.log('=== setGroundColor Start ===');
+        console.log('Raw color value:', colorValue);
+        console.log('Color type:', typeof colorValue);
+        console.log('Color value as string:', String(colorValue));
+        console.log('ThreeJSApp instance:', this === instance ? 'Correct instance' : 'Different instance');
+        
+        if (!this.ground) {
+            console.error('Ground object does not exist');
+            return;
+        }
+        
+        console.log('Ground object exists:', !!this.ground);
+        console.log('Ground object details:', {
+            uuid: this.ground.uuid,
+            type: this.ground.type,
+            visible: this.ground.visible,
+            material: this.ground.material
+        });
+        console.log('Current ground color:', this.ground.material.color);
+        
+        // Dispose of the old material
+        if (this.ground.material) {
+            console.log('Disposing old material:', this.ground.material);
+            this.ground.material.dispose();
+        }
+        
+        // Create new material
+        const newMaterial = new THREE.MeshStandardMaterial({
+            color: colorValue,
+            side: THREE.DoubleSide,
+            roughness: 0.8,
+            metalness: 0.2
+        });
+        
+        // Explicitly set needsUpdate
+        newMaterial.needsUpdate = true;
+        
+        // Assign new material
+        this.ground.material = newMaterial;
+        
+        // Force render
+        console.log('Forcing render...');
+        this.renderer.render(this.scene, this.camera);
+        console.log('Render info:', this.renderer.info);
+        
+        console.log('New material created and assigned:', newMaterial);
+        console.log('New material color:', newMaterial.color);
+        console.log('Material needsUpdate:', newMaterial.needsUpdate); // Should log true
+        console.log('Material properties:', {
+            color: newMaterial.color,
+            side: newMaterial.side,
+            roughness: newMaterial.roughness,
+            metalness: newMaterial.metalness
+        });
+        
+        // Verify ground is in scene
+        const groundInScene = this.scene.children.includes(this.ground);
+        console.log('Is ground in scene?', groundInScene);
+        if (!groundInScene) {
+            console.warn('Ground not in scene, re-adding...');
+            this.scene.add(this.ground);
+        }
+        
+        console.log('Scene children:', this.scene.children.map(child => ({
+            uuid: child.uuid,
+            type: child.type,
+            name: child.name,
+            visible: child.visible
+        })));
+        
+        // Verify animation loop is running
+        console.log('Animation frame ID:', this.animationFrameId);
+        if (!this.animationFrameId) {
+            console.warn('Animation loop not running, restarting...');
+            this.animate();
+        }
+        
+        console.log('=== setGroundColor End ===');
+    }
+
+    setGroundSize(size) {
+        if (this.ground) {
+            this.ground.scale.set(size, 1, size);
+            console.log('Ground size set to:', size);
+        }
+    }
+
+    setCameraPosition(x, y, z) {
+        this.camera.position.set(x, y, z);
+        console.log('Camera position set to:', x, y, z);
+    }
+
+    setCameraLookAt(x, y, z) {
+        this.camera.lookAt(x, y, z);
+        console.log('Camera looking at:', x, y, z);
+    }
+
+    setDirectionalLightColor(color) {
+        if (this.directionalLight) {
+            this.directionalLight.color.set(color);
+            console.log('Directional light color set to:', color);
+        }
+    }
+
+    setDirectionalLightIntensity(intensity) {
+        if (this.directionalLight) {
+            this.directionalLight.intensity = intensity;
+            console.log('Directional light intensity set to:', intensity);
+        }
+    }
+
+    setDirectionalLightPosition(x, y, z) {
+        if (this.directionalLight) {
+            this.directionalLight.position.set(x, y, z);
+            console.log('Directional light position set to:', x, y, z);
+        }
+    }
+
+    setAmbientLightColor(color) {
+        if (this.ambientLight) {
+            this.ambientLight.color.set(color);
+            console.log('Ambient light color set to:', color);
+        }
+    }
+
+    setAmbientLightIntensity(intensity) {
+        if (this.ambientLight) {
+            this.ambientLight.intensity = intensity;
+            console.log('Ambient light intensity set to:', intensity);
+        }
     }
 
     updateDebugDisplay() {
@@ -439,4 +590,84 @@ class ThreeJSApp {
 }
 
 // Make ThreeJSApp available globally
-window.ThreeJSApp = ThreeJSApp; 
+window.ThreeJSApp = ThreeJSApp;
+
+// Make scene operation functions available globally
+let threeApp = null;
+
+// Initialize the application
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('DOM Content Loaded - Initializing ThreeJSApp');
+    if (!threeApp) {
+        threeApp = new ThreeJSApp();
+        console.log('Created new ThreeJSApp instance:', threeApp);
+        threeApp.init();
+    } else {
+        console.log('Using existing ThreeJSApp instance:', threeApp);
+    }
+});
+
+// Global function to get the ThreeJSApp instance
+function getThreeJSApp() {
+    if (!threeApp) {
+        console.warn('ThreeJSApp not initialized, creating new instance');
+        threeApp = new ThreeJSApp();
+        threeApp.init();
+    }
+    return threeApp;
+}
+
+// Update global functions to use getThreeJSApp
+function setGroundColor(color) {
+    console.log('=== Global setGroundColor Start ===');
+    console.log('Global function called with color:', color);
+    const app = getThreeJSApp();
+    console.log('Using ThreeJSApp instance:', app);
+    app.setGroundColor(color);
+    console.log('=== Global setGroundColor End ===');
+}
+
+function setGroundSize(size) {
+    const app = getThreeJSApp();
+    app.setGroundSize(size);
+}
+
+function setCameraPosition(x, y, z) {
+    const app = getThreeJSApp();
+    app.setCameraPosition(x, y, z);
+}
+
+function setCameraLookAt(x, y, z) {
+    const app = getThreeJSApp();
+    app.setCameraLookAt(x, y, z);
+}
+
+function setDirectionalLightColor(color) {
+    const app = getThreeJSApp();
+    app.setDirectionalLightColor(color);
+}
+
+function setDirectionalLightIntensity(intensity) {
+    const app = getThreeJSApp();
+    app.setDirectionalLightIntensity(intensity);
+}
+
+function setDirectionalLightPosition(x, y, z) {
+    const app = getThreeJSApp();
+    app.setDirectionalLightPosition(x, y, z);
+}
+
+function setAmbientLightColor(color) {
+    const app = getThreeJSApp();
+    app.setAmbientLightColor(color);
+}
+
+function setAmbientLightIntensity(intensity) {
+    const app = getThreeJSApp();
+    app.setAmbientLightIntensity(intensity);
+}
+
+function resetScene() {
+    const app = getThreeJSApp();
+    app.resetScene();
+} 
